e17dfc341d98fce8a86785ae209344a0fe5b072c
Anastasia Shadynina <70855182+AnastasiaShad@users.noreply.github.com>
2021-11-01 22:50:03 +0300

    all func

---
+++ client_main2.py
@@ -0,0 +1,130 @@
+import socket, getpass, threading, re, os
+
+# значени хоста и порта по-умолчанию
+HOST = '127.0.0.1'
+# PORT = 63333
+PORT = 65432
+file_ind = 'ind_data2.txt'
+
+
+# проверяем данные ip, port
+def check_inf(ip, port):
+    try:
+        ip = ip.group() if ip else HOST
+        port = int(port) if port != '' else PORT
+        port = port if -1 < port < 65537 else PORT
+        return ip, port
+    except:
+        print("Что-то не так с данными")
+        return False, False
+# получаем данные ip, port
+def inf_user():
+    ip = re.search(r'^\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}$', getpass.getpass(prompt='Введите ip для подключения: '))
+    # ip = ip.group() if ip else HOST
+    port = getpass.getpass(prompt="Введите порт для подключения: ", stream=None)
+    ip, port = check_inf(ip, port)
+    if ip != False or port != False:
+        return ip, port
+    else:
+        while ip == False or port == False:
+            ip = re.search(r'^\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}$',
+                           getpass.getpass(prompt='Введите ip для подключения: '))
+            port = getpass.getpass(prompt='Введите порт для подключения: ')
+            ip, port = check_inf(ip, port)
+            if ip != False and port != False:
+                break
+    return ip, port
+
+
+# получаем данные
+def s_recv(conn):
+    while True:
+        try:
+            msg = conn.recv(1024).decode('utf-8')
+            print(msg)
+        except:
+            print('Отключение')
+            break
+
+
+
+# считываем данные с файла где хранятся индификатор пользователя
+def up_ind():
+    if file_ind in os.listdir(os.getcwd()):
+        with open(file_ind, 'r', encoding ='utf-8') as ss:
+            id_u = ss.read()
+            return id_u if id_u else 'no user_ind'
+    else:
+        a = open(file_ind, 'w')
+        a.close()
+        return 'no user_ind'
+
+# файл для хранения данных о идентификации пользователя
+def ind_file(dta):
+    # file_ind = 'ind_data' + str(dta) + '.txt'
+    with open(file_ind, 'w', encoding ='utf-8') as file:
+        file.write(dta)
+max = 100000
+def main_func():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        ip, port = inf_user()
+        try_connect = 0
+        log = False
+        # если не получается подключить несолько раз, то клиент просто отключается
+        try:
+            while try_connect < 5 and not log:
+                if not log:
+                    # пытаемся подсоединиться
+                    try:
+                        s.connect((ip, port))
+                        print('Соединение успешно было установлено')
+                        log = True
+                    except:
+                        print('Соединение не установлено, повторная попытка')
+                        try_connect += 1
+
+        except:
+            print('Сервер недоступен, было сделано 5 попыток')
+            exit()
+        while True:
+            ms = s.recv(1024).decode('utf-8')
+            if 'name' in ms:
+                s.send(input('Введите имя: ').encode('utf-8'))
+            elif 'password' in ms:
+                name = ms.split(',')[1]
+                s.send(input(f'Введите пароль, {name}: ').encode('utf-8'))
+            elif 'check' in ms:
+                    s.send(input(f'Введите пароль пользователя {ms.split(",")[1]}: ').encode())
+            elif 'user_ind' in ms:
+                tk = up_ind()
+                s.send(tk.encode('utf-8'))
+
+            else:
+                # имя пользователя: сообщения пользователя
+                name = ms.split(',')[1]
+                data_msg = ms.split(',')[2] if len(ms.split(',')) > 2 else False
+                if data_msg:
+                    ind_file(data_msg)
+                break
+        print('exit - выйти')
+        data = s.recv(max).decode('utf-8')
+
+        if data == 'no data':
+            pass
+        else:
+            data = ''.join([i.lstrip() for i in re.split(rf'{name}[:]', data)])[:-1]
+            print(data)
+        # создание потока
+        potok = threading.Thread(target=s_recv, args=(s,))
+        potok.start()
+        while True:
+            text = str(input())
+            if not text or text == 'exit':
+                break
+            s.send(text.encode('utf-8'))
+
+
+
+# запуск основной функции
+if __name__ == '__main__':
+    main_func()
---
+++ server.py
@@ -0,0 +1,302 @@
+import os, socket, getpass, csv, threading, datetime
+from random import randint
+import random
+class Server():
+
+
+
+    log_inf = {1: 'Сервер начал работу', 2: 'Порт слушает', 3: 'Соединение установлено', 4: 'Получение данных',
+               5: 'Клиент был отсоединен',
+               6: 'Сервер был отключен', 7: 'Смена порта', 8: 'Новый клиент', 9: 'Сообщение',
+               10: 'Пауза', 11: 'Показ логов', 12: 'Очистка файла идентификации', 13:'Очистка логов', 14:'Идентификация', 15:'история сообщений', 16: 'Отображение команд',
+               17: 'Отправка данных',
+               18: 'Ввод пароля', 19:'Пользователь подключается'}
+    # значения по-умолчанию
+    HOST = '127.0.0.1'
+    PORT = 65432
+    commands = ['shutdown', 'listen to', 'quit', 'stop port',
+                'show log', 'clear log', 'clear ind', 'help']
+    all_help_commands = ['listen to - прослушивание порта', 'quit - отключение сервера',
+                         'shutdown - отключение клиентов',
+                         'stop port - Пауза (остановка прослушивание порта)',
+                         'show log-Показ логов',
+                         'clear log-Очистка логов',
+                         'clear ind - Очистка файла идентификации',
+                         'help - все команды']
+    listen_ = True
+    # создание уникального ключа для пользователей
+
+    key = ''.join([random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for _ in range(random.randint(1, 10))])
+    # хранение потоков, имен пользователей
+    thread_list = []
+    usern_ames = []
+
+    history = 'history.txt'
+    file_logg = 'log.txt'
+
+
+    def __init__(self, open_port, host):
+        self.open_port = open_port
+        self.host = host
+
+    # смена порта
+    def change_port(self, port):
+        self.open_port = port
+
+    # запись лог файла
+    @staticmethod
+    def log_text(cod):
+        with open(Server.file_logg, 'a') as file:
+            if type(cod) == int:
+                file.write(str(datetime.datetime.now()) + '\t' + Server.log_inf[cod]  +'\n')
+
+
+    @staticmethod
+    def vernam(k, m):
+        k = k*(len(m)//len(k)) + k[-(len(m) % len(k)):]
+        return ''.join(map(chr, [i ^ x for i, x in zip(map(ord, m), map(ord, k))]))
+
+    @staticmethod
+    def check(port_user):
+        try:
+            port_user = int(port_user) if port_user != '' else Server.PORT
+            port_user = port_user if 1 < port_user < 65537 else Server.PORT
+            return port_user
+        except:
+            return False
+    # основная часть
+    @staticmethod
+    def main_program():
+        ClientUser.create_user_file()
+        user_port = getpass.getpass(prompt="Введите порт: ", stream=None)
+        user_port = Server.check(user_port)
+        if user_port == False:
+            while user_port == False:
+                print('Неверный ввод порта')
+                user_port = getpass.getpass(prompt="Введите порт (Enter -- по-умолчанию): ", stream=None)
+                user_port = Server.check(user_port)
+                if user_port != False:
+                    break
+        a = Server(user_port, Server.HOST)
+        a.running_func()
+
+    @staticmethod
+    def listening_inf(sock):
+        while True:
+            if Server.listen_:
+                try:
+                    conn, addr = sock.accept()
+                except:
+                    break
+                # поток пользователя когда получаем данные
+                thread = UserClientThread(len(Server.thread_list), conn, addr)
+                Server.thread_list.append(thread)
+                thread.start()
+            else:
+                continue
+
+    def running_func(self):
+        Server.log_text(1)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            while True:
+                try:
+                    # привязка хоста и порта
+                    s.bind((self.host, self.open_port))
+                    break
+                except:
+                    self.change_port(self.open_port+1)
+                    Server.log_text(7)
+            s.listen(5)
+            # поток
+            thread = threading.Thread(target=Server.listening_inf, args=(s,))
+            Server.thread_list.append(thread)
+            thread.start()
+            Server.log_text(2)
+            print(f'Слушает порт: {self.open_port}')
+            while True:
+                command = input('Введите команду (help - список команд): ')
+                if command == 'shutdown':
+                    Server.log_text(6)
+                    for n, c in Server.usern_ames:
+                        Server.log_text(5)
+                        c.close()
+                    raise SystemExit
+                elif command == "quit":
+                    Server.log_text(6)
+                    raise SystemExit
+                elif command == 'help':
+                    print(', '.join(Server.all_help_commands))
+                elif command == 'listen to':
+                    Server.listen_ = True
+                    Server.log_text(2)
+                    print(f'Слушает порт: {self.open_port}')
+                elif command == 'stop port':
+                    Server.listen_ = False
+                    Server.log_text(10)
+                    print(f'stop port {self.open_port}')
+                elif command == 'clear ind':
+                    Server.log_text(12)
+                    print('Очистка файла идентификации')
+                    ClientUser.user_list.clear()
+                    with open(ClientUser.all_for_users, 'w', encoding='utf-8') as ss:
+                        pass
+                elif command == 'clear log':
+                    print('Очистка логов')
+                    with open(Server.file_logg, 'w') as ss:
+                        pass
+                elif command == 'show log':
+                    print('Логи: ')
+                    Server.log_text(11)
+                    with open(Server.file_logg, 'r') as ss:
+                        text = ss.read()
+                        print(text)
+                elif command != '':
+                    print('Такой команды нет')
+
+# класс для пользователя
+class ClientUser():
+    all_for_users = 'all_for_users.csv'
+    user_list = []
+
+    # создание файла для хранения данных о пользователях
+    @staticmethod
+    def create_user_file():
+        if ClientUser.all_for_users in os.listdir(os.getcwd()):
+            with open(ClientUser.all_for_users, encoding='utf-8') as s:
+                reader = csv.reader(s, delimiter=';')
+                ClientUser.user_list = [row for row in reader]
+        else:
+            a = open(ClientUser.all_for_users, 'w')
+            a.close()
+
+    # добавляем данные о пользователе
+    @staticmethod
+    def write_data_user():
+        with open(ClientUser.all_for_users, 'w', encoding='utf-8') as s:
+            writer = csv.writer(s, delimiter=';')
+            writer.writerows(ClientUser.user_list)
+
+    # создаем уникальный Id
+    @staticmethod
+    def user_ind():
+
+        return ''.join([str(randint(1, 2000)) for i in range(40)])
+
+# поток пользователя
+class UserClientThread(threading.Thread):
+    def __init__(self, name, connector, addr):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.conn = connector
+        self.clientaddr = addr
+
+    def run(self):
+        name = UserClientThread.identify_user(self.conn)
+        Server.usern_ames.append((name, self.conn))
+        Server.log_text(14)
+        self.history_mess()
+        Server.log_text(15)
+        while True:
+            text = UserClientThread.s_recv(self.conn)
+            if not text or text == 'exit':
+                Server.log_text(5)
+                break
+            UserClientThread.s_send(text, name)
+
+    @staticmethod
+    def add_new_user(sock):
+        sock.send('name'.encode('utf-8'))
+        name = sock.recv(1024).decode('utf-8')
+        sock.send(f'password,{name}'.encode('utf-8'))
+        answer = sock.recv(1024).decode('utf-8')
+        key = Server.key
+        password = Server.vernam(key, answer)
+        user_ind = ClientUser.user_ind()
+        sock.send(f'Приветствую пользователя,{name},{user_ind}'.encode('utf-8'))
+        ClientUser.user_list.append([name, password, user_ind, key])
+        ClientUser.write_data_user()
+        Server.log_text(8)
+        return name
+
+    @staticmethod
+    def identify_user(sock):
+        if len(ClientUser.user_list) == 0:
+            return UserClientThread.add_new_user(sock)
+        else:
+            # уникальный инд
+
+
+            # for row in ClientUser.user_list:
+            #     if row[2] == user_ind:
+                    # sock.send(f'Приветствую пользователя,{row[0]}'.encode('utf-8'))
+            # sock.send('name'.encode('utf-8'))
+            # name = sock.recv(1024).decode('utf-8')
+            for i, row in enumerate(ClientUser.user_list):
+                sock.send('user_ind'.encode('utf-8'))
+                user_ind = sock.recv(1024).decode('utf-8')
+                Server.log_text(19)
+                if row[2] == user_ind:
+                    sock.send('name'.encode('utf-8'))
+                    name = sock.recv(1024).decode('utf-8')
+                    while True:
+                        sock.send(f'check,{row[0]}'.encode('utf-8'))
+                        passwd = sock.recv(1024).decode('utf-8')
+                        data = Server.vernam(row[3], passwd)
+                        Server.log_text(19)
+                        if data == row[1]:
+
+                            # user_ind = ClientUser.user_ind()
+                            sock.send(f'Приветствую пользователя,{name},{user_ind}'.encode('utf-8'))
+                            # ClientUser.user_list[i].pop()
+
+                            # ClientUser.user_list[i].append(user_ind)
+                            ClientUser.write_data_user()
+                            return name
+            else:
+                sock.send('name'.encode('utf-8'))
+                name = sock.recv(1024).decode('utf-8')
+                sock.send(f'password,{name}'.encode('utf-8'))
+                answer = sock.recv(1024).decode('utf-8')
+                password = Server.vernam(Server.key, answer)
+                user_ind = ClientUser.user_ind()
+                sock.send(f'Приветствую пользователя,{name},{user_ind}'.encode('utf-8'))
+                key = Server.key
+                ClientUser.user_list.append([name, password, user_ind, key])
+                ClientUser.write_data_user()
+                Server.log_text(8)
+                return name
+
+    # отпрака данных
+    @staticmethod
+    def s_send(mess, name):
+        for name_us, conn in Server.usern_ames:
+            if name_us != name:
+                conn.send(f'{name}:{mess}'.encode('utf-8'))
+        with open(Server.history, 'a') as a:
+            a.write(f'{name}:{mess}\n')
+
+    # получение данных
+    @staticmethod
+    def s_recv(conn):
+        try:
+            text = conn.recv(1024)
+            return text.decode('utf-8')
+        except:
+            pass
+    # история диалога
+    def history_mess(self):
+        if Server.history in os.listdir(os.getcwd()):
+            with open(Server.history, 'r') as ss:
+                file_his = ss.read()
+                if len(file_his) == 0:
+                    self.conn.send('no data'.encode('utf-8'))
+                else:
+                    self.conn.send(f'{file_his}'.encode('utf-8'))
+        else:
+            file_his = open(Server.history, 'w')
+            self.conn.send('no data'.encode('utf-8'))
+            file_his.close()
+
+
+if __name__ == '__main__':
+    Server.main_program()
